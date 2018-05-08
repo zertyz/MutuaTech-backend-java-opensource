76816f27be13b8499e39bdab88435687366c7520
Luiz Silveira <zertyz@gmail.com>
2018-05-08 19:50:02 -0300

    yet starting it...

---
+++ MutuaReactiveHTTPServer/src/mutua/reactive/httpservere/MutuaReactiveHTTPServer.java
@@ -0,0 +1,22 @@
+package mutua.reactive.httpservere;
+
+/** MutuaReactiveHTTPServer.java
+ * =============================
+ * (created by luiz, May 8, 2018)
+ *
+ * This is our Reactive HTTP & S Server implementation, designed to be
+ * as efficient as a server can be. We are tuned to serve applications
+ * using Angular (Universal) Frontend + Java Backend with web sockets
+ * connection.
+ * 
+ * Reactive programming is all about developing scalable, non-blocking, event driven
+ * and low latency services and this is our version of a Highly Efficient and KISS
+ * HTTP & S Server.
+ *
+ * @see RelatedClass(es)...
+ * @author luiz
+*/
+
+public class MutuaReactiveHTTPServer {
+
+}
