b0647072fdfe6ae6ec8b4381cfcfdced890630a8
Luiz Silveira <zertyz@gmail.com>
2018-05-08 17:25:32 -0300

    welcome, mutua reactive http server

---
+++ MutuaReactiveHTTPServerTests/src/mutua/reactive/httpserver/MutuaReactiveHTTPServerTests.java
@@ -0,0 +1,14 @@
+package mutua.reactive.httpserver;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MutuaReactiveHTTPServerTests {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
